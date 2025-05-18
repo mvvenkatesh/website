3e8ccbb09777f2bad2c9c8543b001d17f6d6895f
venkatesh <vnagasamudram@uco.edu>
2025-05-18 00:43:15 -0500

    website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DetailsComponent } from "./details/details.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DetailsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
